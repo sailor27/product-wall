b8099cd0f7d48dd1db2c8cd2c273228a9e84a96a
Sailor Winkelman <sailor.winkelman@nike.com>
2019-10-13 01:20:56 -0700

    "Create react app and display product data from API using fetch"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      products: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true})
+    fetch(`https://api.bestbuy.com/v1/products(search=oven&search=stainless&search=steel)?format=json&show=all&apiKey=6cvpy87wue568w6cpzjy4dhk`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => this.setState({
+        products: data.products,
+        isLoading: false
+      }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { products, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+    return (
+      <div className="App">
+        {products.map((product, i)=> (
+          <Fragment key={i}>
+            <div>{product.name}</div>
+            <div>{product.regularPrice}</div>
+            <div>{product.longDescription}</div>
+          </Fragment>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
